1f94cd205b5a2ce07ec17c7520d9d4089c7110d5
digimori <hanaorikuu@gmail.com>
2023-04-30 20:32:04 +0000

    Cart app created, basic view rendered. cart.html basic table and if statement created

---
+++ cart/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def view_cart(request):
+    """ A view to return the shopping cart page """
+
+    return render(request, 'cart/cart.html')
+
+
+def add_items(request, item_id):
+    pass
--- cottagedelights/settings.py
+++ cottagedelights/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'storages',
+    'cart',
 
 ]
 
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
                 'django.template.context_processors.media',
             ],
         },
@@ -152,6 +154,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 30
+STANDARD_DELIVERY_PERCENTAGE = 5
 
 if 'USE_AWS' in os.environ:
     # AWS Cache Control:
