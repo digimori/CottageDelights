c4857cfd286a31528ca2b30a99c432ab99e375d9
digimori <hanaorikuu@gmail.com>
2023-04-30 10:02:07 +0000

    Heroku troubleshooting - CollectStatic

--- custom_storages.py
+++ custom_storages.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorage(S33BotoStorage):
+class StaticStorage(S3BotoStorage):
     location = settings.STATICFILES_LOCATION
 
 
