c41b0297d931b6250666c33fbe25ec62fdb3d91b
digimori <hanaorikuu@gmail.com>
2023-04-30 11:07:54 +0000

    Base Product query added to all_products view.

--- products/views.py
+++ products/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from .models import Product
+from django.contrib import messages
+from django.db.models import Q
 
 
 def all_products(request):
     products = Product.objects.all()
 
+    query = None
+    if request.GET:
+        if 'q' in request.GET['q']:
+            if not query:
+                messages.error(
+                    request, 'Please enter valid search criteria')
+                return redirect(reverse, 'products')
+
+            queries = Q(
+                name__, contains=query) | Q(description__, contains=query)
+            products = products.filter(queries)
+
     context = {
-        'products': products
+        'products': products,
+        'search_term': query
     }
     return render(request, 'products/products.html', context)
 
