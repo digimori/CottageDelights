a97192ca4270b4763797bec8eb1997b60f1e59b1
digimori <hanaorikuu@gmail.com>
2023-04-30 19:29:23 +0000

    Product sorting added. Links in the main nav now separate out the categories assigned in json files

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from .models import Product
+from .models import Product, Category
 from django.contrib import messages
 from django.db.models import Q
 
@@ -8,20 +8,29 @@ def all_products(request):
     products = Product.objects.all()
 
     query = None
+    categories = None
+
     if request.GET:
-        if 'q' in request.GET['q']:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
             if not query:
                 messages.error(
-                    request, 'Please enter valid search criteria')
-                return redirect(reverse, 'products')
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
 
             queries = Q(
-                name__, contains=query) | Q(description__, contains=query)
+                name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     context = {
         'products': products,
-        'search_term': query
+        'search_term': query,
+        'current_categories': categories
     }
     return render(request, 'products/products.html', context)
 
