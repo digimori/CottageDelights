aa7ed62a34795be62f3548b300c6aa6fd3e39a70
digimori <hanaorikuu@gmail.com>
2023-04-30 21:46:10 +0000

    Forgot to add imports to wishlist/views.py

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
+
+
+def wishlist(request):
+    """ A view to return the index page """
+
+    return render(request, 'wishlist/wishlist.html')
