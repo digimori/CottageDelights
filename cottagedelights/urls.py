5858f6752ddf187739e318350feecdb5148d9598
digimori <hanaorikuu@gmail.com>
2023-04-30 21:43:22 +0000

    Wishlist base template, views and urls added

--- cottagedelights/urls.py
+++ cottagedelights/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
+    path('wishlist/', include('wishlist.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
