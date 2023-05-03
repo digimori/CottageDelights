7b947c8dc011d7bd1259149e845908df774b523c
digimori <hanaorikuu@gmail.com>
2023-05-03 18:43:34 +0000

    Add to Wishlist button added - Not functional, but views have been added to wishlists. Crispyforms installed to terminal

--- wishlist/views.py
+++ wishlist/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 from products.models import Product
 
-# Create your views here.
-
 
 def wishlist(request):
-    """ A view to return the index page """
+    """ A view to return the wishlist page """
 
     return render(request, 'wishlist/wishlist.html')
 
@@ -14,7 +12,7 @@ def wishlist(request):
 def add_to_wishlist(request, item_id):
     product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+    redirect_url_wish = request.POST.get('redirect_url')
     wishlist = request.session.get('wishlist', {})
 
     if item_id in list(wishlist.keys()):
@@ -26,3 +24,29 @@ def add_to_wishlist(request, item_id):
     request.session['wishlist'] = wishlist
 
     return redirect(redirect_url)
+
+
+def adjust_wishlist(request, item_id):
+    quantity = int(request.POST.get('quantity'))
+    wishlist = request.session.get('wishlist', {})
+
+    if quantity > 0:
+        wishlist[item_id] = quantity
+    else:
+        wishlist.pop[item_id]
+
+    request.session['wishlist'] = wishlist
+    return redirect(reverse('view_wishlist'))
+
+
+def remove_from_wishlist(request, item_id):
+    """Remove the item from wishlist"""
+
+    product = Product.objects.get(pk=item_id)
+    redirect_url = request.POST.get('redirect_url')
+    wishlist = request.session.get('wishlist', {})
+
+    wishlist.pop(item_id)
+    messages.success(request, f'Removed {product.name} from your wishlist')
+
+    return redirect(reverse('view_wishlist'))
