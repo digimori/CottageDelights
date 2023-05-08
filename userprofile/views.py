00ddd7865f77dfa83899fb049b9a2426fd2db5b9
digimori <hanaorikuu@gmail.com>
2023-05-08 15:31:12 +0000

    Reconfigured the userprofile page - Still need to fix Webhooks - Go back over the course material section in Stripe

--- userprofile/views.py
+++ userprofile/views.py
@@ -6,24 +6,11 @@ from .forms import UserProfileForm
 
 
 # Create your views here.
-def user_profile(request):
+def userprofile(request):
     """ View to return a profile page """
-    profile = get_object_or_404(UserProfile, user=request.user)
-
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
 
     template = 'userprofile/profile.html'
     context = {
-        'form': form,
-        'orders': orders,
-        'on_profile_page': True
     }
 
     return render(request, template, context)
