3f954446da8cc658fffc22e2625701beb2a91a52
digimori <hanaorikuu@gmail.com>
2023-05-13 11:47:14 +0000

    Fixed typo on order total as it was not displaying - some templates had 'order' and others had 'order_total' so were not matching up with the model and therefore not calculating

--- cart/contexts.py
+++ cart/contexts.py
@@ -7,14 +7,14 @@ from products.models import Product
 def cart_contents(request):
 
     cart_items = []
-    total = 0
+    order_total = 0
     product_quant = 0
     cart = request.session.get('cart', {})
 
     for item_id, item_data in cart.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
+            order_total += item_data * product.price
             product_quant += item_data
             cart_items.append({
                 'item_id': item_id,
@@ -24,18 +24,18 @@ def cart_contents(request):
         else:
             product = get_object_or_404(Product, pk=item_id)
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    if order_total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = order_total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - order_total
     else:
         delivery = 0
         free_delivery_delta = 0
 
-    grand_total = delivery + total
+    grand_total = delivery + order_total
 
     context = {
         'cart_items': cart_items,
-        'total': total,
+        'order_total': order_total,
         'product_quant': product_quant,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
