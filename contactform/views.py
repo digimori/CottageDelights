60f8d32ee9311cdfc647f2eca7b31f0c56877f2e
digimori <hanaorikuu@gmail.com>
2023-06-04 17:54:27 +0000

    Contact form View created. Basic form populated on contact.html page

--- contactform/views.py
+++ contactform/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from .models import ContactForm
 
 
 def ContactForm(request):
-    """ A view to return the contact page """
+    """ Contact Form Submission """
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        order_number = request.POST.get("order_number")
+        description = request.POST.get("description")
+        query = ContactForm(
+            name=name,
+            email=email,
+            order_number=order_number,
+            description=description)
+        query.save()
+        messages.info(
+            request,
+            "Thanks for your message, we will respond soon!")
+        return redirect('/contactform')
 
     return render(request, 'contactform/contact.html')
