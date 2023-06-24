0e05a21b851855ff5f8e9e1ba523298b7fb19680
digimori <hanaorikuu@gmail.com>
2023-06-24 17:25:49 +0000

    Newsletter troubleshoot - Reverted App Config changes

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.NewsletterSub, name='NewsletterSub'),
+    path('', views.NewsletterSub, name='newsletter'),
     path('mailmessage/', views.MailMessage, name='MailMessage')
 ]
