9903bea1188383172174c872161ae7a84b5c6ed9
digimori <hanaorikuu@gmail.com>
2023-06-24 17:30:46 +0000

    Newsletter troubleshooting

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.NewsletterSub, name='newsletter'),
+    path('', views.NewsletterSub, name='NewsletterSub'),
     path('mailmessage/', views.MailMessage, name='MailMessage')
 ]
